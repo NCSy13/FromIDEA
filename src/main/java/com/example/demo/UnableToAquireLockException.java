0306b8c76af81f952f29a681e68740ff71a3f1a9
yanbin <yanbin@ncsi.com.cn>
2021-05-31 19:30:19 +0800

    update

---
+++ src/main/java/com/example/demo/UnableToAquireLockException.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+/**
+ * 异常类
+ */
+public class UnableToAquireLockException extends RuntimeException{
+
+    public UnableToAquireLockException() {
+    }
+
+    public UnableToAquireLockException(String message) {
+        super(message);
+    }
+
+    public UnableToAquireLockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
